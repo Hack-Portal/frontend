64eebe9d180a9270e683940bb77c06875ee677b9
hiroto2002 <hiroto2002h14@icloud.com>
2023-07-06 23:51:49 +0900

    feat:enviroment

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+const nextJest = require("next/jest");
+
+const createJestConfig = nextJest({
+  // next.config.jsとテスト環境用の.envファイルが配置されたディレクトリをセット。基本は"./"で良い。
+  dir: "./",
+});
+
+const customJestConfig = {
+  moduleNameMapper: {
+    "^@/components/(.*)$": "<rootDir>/components/$1",
+    "^@/pages/(.*)$": "<rootDir>/pages/$1",
+  },
+  testEnvironment: "jest-environment-jsdom",
+};
+
+module.exports = createJestConfig(customJestConfig);
