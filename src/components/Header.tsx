377c56528a410a264a38a815aa0c2ae960b3083c
tak <taku1011ht@gmail.com>
2023-07-09 03:56:02 +0900

    HeaderLinkFix

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,27 +1,23 @@
-import * as React from 'react';
-import { ColorPaletteProp } from '@mui/joy/styles';
-import Badge from '@mui/joy/Badge';
-import Box from '@mui/joy/Box';
-import Input from '@mui/joy/Input';
-import IconButton from '@mui/joy/IconButton';
-import Typography from '@mui/joy/Typography';
-import ListItem from '@mui/joy/ListItem';
-import MailOutlineIcon from '@mui/icons-material/MailOutline';
-import { Avatar } from '@mui/joy';
-import Image from 'next/image';
-import PermIdentityIcon from '@mui/icons-material/PermIdentity';
-import Link from 'next/link';
-import { Button } from '@mui/material';
-import Sheet from '@mui/joy/Sheet';
-import GroupsIcon from '@mui/icons-material/Groups';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import NotificationsNoneOutlinedIcon from '@mui/icons-material/NotificationsNoneOutlined';
-
-
-
-
+import * as React from 'react'
+import { ColorPaletteProp } from '@mui/joy/styles'
+import Badge from '@mui/joy/Badge'
+import Box from '@mui/joy/Box'
+import Input from '@mui/joy/Input'
+import IconButton from '@mui/joy/IconButton'
+import Typography from '@mui/joy/Typography'
+import ListItem from '@mui/joy/ListItem'
+import MailOutlineIcon from '@mui/icons-material/MailOutline'
+import { Avatar } from '@mui/joy'
+import Image from 'next/image'
+import PermIdentityIcon from '@mui/icons-material/PermIdentity'
+import Link from 'next/link'
+import { Button } from '@mui/material'
+import Sheet from '@mui/joy/Sheet'
+import GroupsIcon from '@mui/icons-material/Groups'
+import ListItemButton from '@mui/material/ListItemButton'
+import ListItemIcon from '@mui/material/ListItemIcon'
+import ListItemText from '@mui/material/ListItemText'
+import NotificationsNoneOutlinedIcon from '@mui/icons-material/NotificationsNoneOutlined'
 
 export const Header = () => {
   return (
@@ -39,12 +35,12 @@ export const Header = () => {
       }}
     >
       <Link href="/">
-      <ListItemButton sx={{borderRadius: '13%'}}>
-      <GroupsIcon sx={{ height:50,width:50}}/>
-      </ListItemButton>
+        <ListItemButton sx={{ borderRadius: '13%' }}>
+          <GroupsIcon sx={{ height: 50, width: 50 }} />
+        </ListItemButton>
       </Link>
-      
-      <Box 
+
+      <Box
         sx={{
           position: 'flex',
           top: 0,
@@ -54,39 +50,47 @@ export const Header = () => {
           alignItems: 'center',
           py: 2,
           px: 1,
-          
         }}
       >
-    <Link  href="/hackathon">
-    <ListItem sx={{width:142}}>
-            <ListItemButton sx={{borderRadius: '5%'}}>
-            <ListItemText sx={{  fontFamily:"bold",textAlign: "center"}} primary="ハッカソン" />
+        <Link href="/hackathon">
+          <ListItem sx={{ width: 142 }}>
+            <ListItemButton sx={{ borderRadius: '5%' }}>
+              <ListItemText
+                sx={{ fontFamily: 'bold', textAlign: 'center' }}
+                primary="ハッカソン"
+              />
             </ListItemButton>
-        </ListItem>
-    </Link>
+          </ListItem>
+        </Link>
 
-    <Link  href="/ranking">
-    <ListItem sx={{width:142}}>
-            <ListItemButton  sx={{borderRadius: '5%'}}>
-            <ListItemText  sx={{ fontFamily:"bold",textAlign: "center"}} primary="ランキング" />
+        <Link href="/ranking">
+          <ListItem sx={{ width: 142 }}>
+            <ListItemButton sx={{ borderRadius: '5%' }}>
+              <ListItemText
+                sx={{ fontFamily: 'bold', textAlign: 'center' }}
+                primary="ランキング"
+              />
             </ListItemButton>
-        </ListItem>
-    </Link>
+          </ListItem>
+        </Link>
 
-    <Link  href="/member">
-    <ListItem sx={{width:142 ,  textAlign: "center"}}>
-            <ListItemButton  sx={{borderRadius: '5%'}} >
-            <ListItemText sx={{ fontFamily:"bold" }} primary="メンバー募集" />
+        <Link href="/member">
+          <ListItem sx={{ width: 142, textAlign: 'center' }}>
+            <ListItemButton sx={{ borderRadius: '5%' }}>
+              <ListItemText
+                sx={{ fontFamily: 'bold' }}
+                primary="メンバー募集"
+              />
             </ListItemButton>
-        </ListItem>
-    </Link>
+          </ListItem>
+        </Link>
       </Box>
-      
+
       <Box
-        sx={{ 
-          display: 'flex', 
-          flexDirection: 'row-reverse', 
-          flexShrink: 0 
+        sx={{
+          display: 'flex',
+          flexDirection: 'row-reverse',
+          flexShrink: 0,
         }}
       >
         <Box sx={{ display: 'flex', gap: 2 }}>
@@ -94,34 +98,41 @@ export const Header = () => {
             placeholder="Search for anything..."
             variant="soft"
             size="sm"
-            sx={{solid: '6px #e0e0e0',
+            sx={{
+              solid: '6px #e0e0e0',
               '--Input-radius': '40px',
               '--Input-paddingInline': '12px',
               width: 300,
               display: { xs: 'none', lg: 'flex' },
             }}
           />
-          
+
           <Box sx={{ display: 'flex', marginRight: 5, marginLeft: 5 }}>
-          <Link href="/notification">
-            <Badge badgeContent={2} variant="solid" color="danger">
-              <NotificationsNoneOutlinedIcon
-                sx={{ width: 30, height: 30 ,marginRight: 1, marginLeft: 1}}
+            <Link href="/notification">
+              <Badge badgeContent={2} variant="solid" color="danger">
+                <NotificationsNoneOutlinedIcon
+                  sx={{ width: 30, height: 30, marginRight: 1, marginLeft: 1 }}
+                  color="info"
+                />
+              </Badge>
+            </Link>
+            <Link href="/dm">
+              <Badge badgeContent={2} variant="solid" color="danger">
+                <MailOutlineIcon
+                  sx={{ width: 30, height: 30, marginRight: 1, marginLeft: 1 }}
+                  color="info"
+                />
+              </Badge>
+            </Link>
+            <Link href="/user">
+              <PermIdentityIcon
+                sx={{ width: 30, height: 30, marginRight: 1, marginLeft: 1 }}
                 color="info"
               />
-            </Badge>
-          </Link>
-          <Link href="/dm">
-            <Badge badgeContent={2} variant="solid" color="danger">
-              <MailOutlineIcon sx={{ width: 30, height: 30 ,marginRight: 1, marginLeft: 1 }} color="info" />
-            </Badge>
-          </Link>
-          <Link href="/user">
-            <PermIdentityIcon sx={{ width: 30, height: 30,marginRight: 1, marginLeft: 1 }} color="info" />
-          </Link>
+            </Link>
           </Box>
         </Box>
       </Box>
     </Sheet>
-  );
-};
+  )
+}
--- src/components/HeaderLink.jsx
+++ src/components/HeaderLink.jsx
@@ -1,16 +1,19 @@
 import React from 'react'
 import { ListItem, ListItemButton, ListItemText,Link } from '@mui/material'
-
+//分割しようとしたけど反映失敗したので保留
 export const HeaderLink = (name,link) => {
   return (
     <>
-    <Link  href={link}>
-    <ListItem sx={{width:100}}>
-            <ListItemButton>
-            <ListItemText fontWeight="bold" sx={{ ml: 2 }} primary={name} />
+        <Link href={link}>
+          <ListItem sx={{ width: 142, textAlign: 'center' }}>
+            <ListItemButton sx={{ borderRadius: '5%' }}>
+              <ListItemText
+                sx={{ fontFamily: 'bold' }}
+                primary={name}
+              />
             </ListItemButton>
-        </ListItem>
-    </Link>
+          </ListItem>
+        </Link>
     </>
   )
 }
