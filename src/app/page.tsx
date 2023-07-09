f3c2fd23e3431da2e718f6560c7810b940e9a442
tak <taku1011ht@gmail.com>
2023-07-09 17:43:19 +0900

    AuthFormCreate

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
-"use client";
+'use client'
 import { Header } from '@/components/Header'
-
+import { LoginForm } from '@/components/LoginForm'
+import { RegisterForm } from '@/components/RegisterForm'
 export default function Home() {
   return (
-    <main style={{background:"white"}}>
-     <Header/>
+    <main style={{ background: 'white' }}>
+      {/* <Header /> */}
+      {/* <LoginForm /> */}
+      <RegisterForm />
     </main>
   )
 }
---
+++ src/components/layouts/FormWrap.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { Box, Button, TextField } from '@mui/material'
+
+export const FormWrap = (children) => {
+  return
+  ;<Box sx={{ background: 'blue', width: '800px', height: ' 600px' }}>
+    {children}
+  </Box>
+}
