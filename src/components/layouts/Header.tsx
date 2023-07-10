db3c5e20b270655f7f78f65a05dbd6151ee3785e
tak <taku1011ht@gmail.com>
2023-07-10 23:31:05 +0900

    HeaderFix

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -3,7 +3,7 @@ import { ColorPaletteProp } from '@mui/joy/styles'
 import Badge from '@mui/joy/Badge'
 import Box from '@mui/joy/Box'
 import Input from '@mui/joy/Input'
-import IconButton from '@mui/joy/IconButton'
+
 import Typography from '@mui/joy/Typography'
 import ListItem from '@mui/joy/ListItem'
 import MailOutlineIcon from '@mui/icons-material/MailOutline'
@@ -11,7 +11,7 @@ import { Avatar } from '@mui/joy'
 import Image from 'next/image'
 import PermIdentityIcon from '@mui/icons-material/PermIdentity'
 import Link from 'next/link'
-import { Button } from '@mui/material'
+import { Button, GridListTileBar, IconButton } from '@mui/material'
 import Sheet from '@mui/joy/Sheet'
 import GroupsIcon from '@mui/icons-material/Groups'
 import ListItemButton from '@mui/material/ListItemButton'
@@ -21,119 +21,149 @@ import NotificationsNoneOutlinedIcon from '@mui/icons-material/NotificationsNone
 
 export const Header = () => {
   return (
-    <Sheet
-      color="primary"
-      sx={{
-        position: 'fixed',
-        top: 0,
-        left: 0,
-        width: '100%',
-        display: 'flex',
-        alignItems: 'center',
-        py: 2,
-        px: 1,
-        zIndex: 100,
-      }}
-    >
-      <Link href="/">
-        <ListItemButton sx={{ borderRadius: '13%' }}>
-          <GroupsIcon sx={{ height: 50, width: 50 }} />
-        </ListItemButton>
-      </Link>
-
+    <>
       <Box
         sx={{
-          position: 'flex',
           top: 0,
           left: 0,
           width: '100%',
+          height: '12vh ',
+          alignItems: 'center',
+          py: 2,
+          px: 1,
+        }}
+      />
+      <Sheet
+        color="primary"
+        sx={{
+          position: 'fixed',
+          zIndex: 200,
+          top: 0,
+          left: 0,
+          width: '100%',
+          height: '10%',
           display: 'flex',
           alignItems: 'center',
           py: 2,
           px: 1,
         }}
       >
-        <Link href="/hackathon">
-          <ListItem sx={{ width: 142 }}>
-            <ListItemButton sx={{ borderRadius: '5%' }}>
-              <ListItemText
-                sx={{ fontFamily: 'bold', textAlign: 'center' }}
-                primary="ハッカソン"
-              />
-            </ListItemButton>
-          </ListItem>
+        <Link href="/">
+          <ListItemButton sx={{ borderRadius: '13%' }}>
+            <GroupsIcon sx={{ height: 50, width: 50 }} />
+          </ListItemButton>
         </Link>
 
-        <Link href="/ranking">
-          <ListItem sx={{ width: 142 }}>
-            <ListItemButton sx={{ borderRadius: '5%' }}>
-              <ListItemText
-                sx={{ fontFamily: 'bold', textAlign: 'center' }}
-                primary="ランキング"
-              />
-            </ListItemButton>
-          </ListItem>
-        </Link>
+        <Box
+          sx={{
+            position: 'sticky',
+            top: 0,
+            left: 0,
+            width: '100%',
+            display: 'flex',
+            alignItems: 'center',
+            py: 2,
+            px: 1,
+          }}
+        >
+          <Link href="/hackathon">
+            <ListItem sx={{ width: 142 }}>
+              <ListItemButton sx={{ borderRadius: '5%' }}>
+                <ListItemText
+                  sx={{ fontFamily: 'bold', textAlign: 'center' }}
+                  primary="ハッカソン"
+                />
+              </ListItemButton>
+            </ListItem>
+          </Link>
 
-        <Link href="/member">
-          <ListItem sx={{ width: 142, textAlign: 'center' }}>
-            <ListItemButton sx={{ borderRadius: '5%' }}>
-              <ListItemText
-                sx={{ fontFamily: 'bold' }}
-                primary="メンバー募集"
-              />
-            </ListItemButton>
-          </ListItem>
-        </Link>
-      </Box>
+          <Link href="/ranking">
+            <ListItem sx={{ width: 142 }}>
+              <ListItemButton sx={{ borderRadius: '5%' }}>
+                <ListItemText
+                  sx={{ fontFamily: 'bold', textAlign: 'center' }}
+                  primary="ランキング"
+                />
+              </ListItemButton>
+            </ListItem>
+          </Link>
 
-      <Box
-        sx={{
-          display: 'flex',
-          flexDirection: 'row-reverse',
-          flexShrink: 0,
-        }}
-      >
-        <Box sx={{ display: 'flex', gap: 2 }}>
-          <Input
-            placeholder="Search for anything..."
-            variant="soft"
-            size="sm"
-            sx={{
-              solid: '6px #e0e0e0',
-              '--Input-radius': '40px',
-              '--Input-paddingInline': '12px',
-              width: 300,
-              display: { xs: 'none', lg: 'flex' },
-            }}
-          />
+          <Link href="/member">
+            <ListItem sx={{ width: 142, textAlign: 'center' }}>
+              <ListItemButton sx={{ borderRadius: '5%' }}>
+                <ListItemText
+                  sx={{ fontFamily: 'bold' }}
+                  primary="メンバー募集"
+                />
+              </ListItemButton>
+            </ListItem>
+          </Link>
+        </Box>
 
-          <Box sx={{ display: 'flex', marginRight: 5, marginLeft: 5 }}>
-            <Link href="/notification">
-              <Badge badgeContent={2} variant="solid" color="danger">
-                <NotificationsNoneOutlinedIcon
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'row-reverse',
+            flexShrink: 0,
+          }}
+        >
+          <Box sx={{ display: 'flex', gap: 2 }}>
+            <Input
+              placeholder="Search for anything..."
+              variant="soft"
+              size="sm"
+              sx={{
+                solid: '6px #e0e0e0',
+                '--Input-radius': '40px',
+                '--Input-paddingInline': '12px',
+                width: 300,
+                display: { xs: 'none', lg: 'flex' },
+              }}
+            />
+
+            <Box sx={{ display: 'flex', marginRight: 5, marginLeft: 5 }}>
+              <ListItem>
+                <ListItemButton>
+                  <Link href="/notification">
+                    <Badge badgeContent={2} variant="solid" color="danger">
+                      <NotificationsNoneOutlinedIcon
+                        sx={{
+                          width: 30,
+                          height: 30,
+                          marginRight: 1,
+                          marginLeft: 1,
+                        }}
+                        color="info"
+                      />
+                    </Badge>
+                  </Link>
+                </ListItemButton>
+              </ListItem>
+
+              <Link href="/dm">
+                <Badge badgeContent={2} variant="solid" color="danger">
+                  <MailOutlineIcon
+                    sx={{
+                      width: 30,
+                      height: 30,
+                      marginRight: 1,
+                      marginLeft: 1,
+                    }}
+                    color="info"
+                  />
+                </Badge>
+              </Link>
+
+              <Link href="/user">
+                <PermIdentityIcon
                   sx={{ width: 30, height: 30, marginRight: 1, marginLeft: 1 }}
                   color="info"
                 />
-              </Badge>
-            </Link>
-            <Link href="/dm">
-              <Badge badgeContent={2} variant="solid" color="danger">
-                <MailOutlineIcon
-                  sx={{ width: 30, height: 30, marginRight: 1, marginLeft: 1 }}
-                  color="info"
-                />
-              </Badge>
-            </Link>
-            <Link href="/user">
-              <PermIdentityIcon
-                sx={{ width: 30, height: 30, marginRight: 1, marginLeft: 1 }}
-                color="info"
-              />
-            </Link>
+              </Link>
+            </Box>
           </Box>
         </Box>
-      </Box>
-    </Sheet>
+      </Sheet>
+    </>
   )
 }
