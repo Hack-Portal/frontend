a15161de80097e15de3a0db2c485ea66175b4177
tak <taku1011ht@gmail.com>
2023-07-15 17:28:27 +0900

    hackathons_dataFix

--- next.config.js
+++ next.config.js
@@ -1,14 +1,8 @@
 /** @type {import('next').NextConfig} */
-import firebase from 'firebase/app'
-import 'firebase/auth'
+
 
 const config = {
-  apiKey: 'your-api-key',
-  authDomain: 'your-auth-domain',
-  projectId: 'your-project-id',
-  storageBucket: 'your-storage-bucket',
-  messagingSenderId: 'your-messaging-sender-id',
-  appId: 'your-app-id'
+
 }
 
 if (!firebase.apps.length) {
@@ -17,3 +11,5 @@ if (!firebase.apps.length) {
 
 export default firebase
 module.exports = nextConfig
+
+  
