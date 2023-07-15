d27a89ada55977749e85840c254187a644d97e94
hiroto2002 <hiroto2002h14@icloud.com>
2023-07-15 16:02:09 +0900

    fix:home design

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -11,13 +11,31 @@ import GroupsIcon from '@mui/icons-material/Groups'
 import ListItemButton from '@mui/material/ListItemButton'
 import ListItemText from '@mui/material/ListItemText'
 import NotificationsNoneOutlinedIcon from '@mui/icons-material/NotificationsNoneOutlined'
+import Image from 'next/image'
+import { Grid, Typography } from '@mui/material'
+import { useCustomRouter } from './hooks/CustomRouter'
 
 export const Header = () => {
+  const NAV_ITEMS = [
+    {
+      label: 'ハッカソン',
+      href: '/',
+    },
+    {
+      label: 'ランキング',
+      href: '/ranking',
+    },
+    {
+      label: 'メンバー募集',
+      href: '/room',
+    },
+  ]
+
+  const { IsActive } = useCustomRouter()
+
   return (
     <Box
       sx={{
-        top: 0,
-        left: 0,
         width: '100%',
         height: '75px ',
         py: 2,
@@ -35,119 +53,92 @@ export const Header = () => {
           height: '10%',
           display: 'flex',
           alignItems: 'center',
-          py: 2,
-          px: 1,
+          justifyContent: 'space-between',
+          py: 3,
+          px: 3,
+          boxShadow: '0px 0px 5px 0px rgba(0,0,0,0.2)',
         }}
       >
-        <Link href="/">
-          <ListItemButton sx={{ borderRadius: '13%' }}>
-            <GroupsIcon sx={{ height: 50, width: 50 }} />
-          </ListItemButton>
-        </Link>
         <Box
           sx={{
-            position: 'sticky',
-            top: 0,
-            left: 0,
-            width: '100%',
             display: 'flex',
             alignItems: 'center',
-            py: 2,
-            px: 1,
           }}
         >
-          <Link href="/">
-            <ListItem sx={{ width: 142 }}>
-              <ListItemButton sx={{ borderRadius: '5%' }}>
-                <ListItemText
-                  sx={{ fontFamily: 'bold', textAlign: 'center' }}
-                  primary="ハッカソン"
-                />
-              </ListItemButton>
-            </ListItem>
+          <Link href="/" style={{ margin: '0 16px 0 0' }}>
+            <Image
+              src={'/image/logo.svg'}
+              alt="logo"
+              width={87.5}
+              height={35}
+            />
           </Link>
-
-          <Link href="/ranking">
-            <ListItem sx={{ width: 142 }}>
-              <ListItemButton sx={{ borderRadius: '5%' }}>
-                <ListItemText
-                  sx={{ fontFamily: 'bold', textAlign: 'center' }}
-                  primary="ランキング"
-                />
-              </ListItemButton>
-            </ListItem>
-          </Link>
-
-          <Link href="/room">
-            <ListItem sx={{ width: 142, textAlign: 'center' }}>
-              <ListItemButton sx={{ borderRadius: '5%' }}>
-                <ListItemText
-                  sx={{ fontFamily: 'bold' }}
-                  primary="メンバー募集"
-                />
-              </ListItemButton>
-            </ListItem>
-          </Link>
+          {NAV_ITEMS.map((item, index) => (
+            <Link
+              href={item.href}
+              key={index}
+              style={{
+                borderBottom: IsActive(item.href)
+                  ? 'solid 2px #0288d1'
+                  : 'none',
+                padding: '6px 0 0 0',
+              }}
+            >
+              <ListItem>
+                <ListItemButton sx={{ borderRadius: '5%' }}>
+                  <ListItemText
+                    primary={
+                      <Typography
+                        style={{
+                          fontWeight:IsActive(item.href) ? 'bold' : 'normal',
+                          color: IsActive(item.href) ? '#333' : '#aaa',
+                        }}
+                      >
+                        {item.label}
+                      </Typography>
+                    }
+                    sx={{}}
+                  />
+                </ListItemButton>
+              </ListItem>
+            </Link>
+          ))}
         </Box>
 
-        <Box
-          sx={{
-            display: 'flex',
-            flexDirection: 'row-reverse',
-            flexShrink: 0,
-          }}
-        >
-          <Box sx={{ display: 'flex', gap: 2 }}>
-            <Box sx={{ display: 'flex', marginRight: 5, marginLeft: 5 }}>
-              <ListItem>
-                <ListItemButton sx={{ borderRadius: '15%' }}>
-                  <Link href="/notification">
-                    <Badge badgeContent={2} variant="solid" color="danger">
-                      <NotificationsNoneOutlinedIcon
-                        sx={{
-                          width: 30,
-                          height: 30,
-                        }}
-                        color="info"
-                      />
-                    </Badge>
-                  </Link>
-                </ListItemButton>
-              </ListItem>
+        <Box sx={{display:"flex",gap:3}}>
+          <Link href="/notification">
+            {/* <Badge badgeContent={2} variant="solid" color="danger"> */}
+            <NotificationsNoneOutlinedIcon
+              sx={{
+                width: 30,
+                height: 30,
+              }}
+              color="info"
+            />
+            {/* </Badge> */}
+          </Link>
 
-              <ListItem>
-                <ListItemButton sx={{ borderRadius: '15%' }}>
-                  <Link href="/joinedRoom">
-                    <Badge badgeContent={2} variant="solid" color="danger">
-                      <MailOutlineIcon
-                        sx={{
-                          width: 30,
-                          height: 30,
-                        }}
-                        color="info"
-                      />
-                    </Badge>
-                  </Link>
-                </ListItemButton>
-              </ListItem>
+          <Link href="/joinedRoom">
+            {/* <Badge badgeContent={2} variant="solid" color="danger"> */}
+            <MailOutlineIcon
+              sx={{
+                width: 30,
+                height: 30,
+              }}
+              color="info"
+            />
+            {/* </Badge> */}
+          </Link>
 
-              <ListItem>
-                <ListItemButton sx={{ borderRadius: '15%' }}>
-                  <Link href="/user">
-                    <PermIdentityIcon
-                      sx={{
-                        width: 30,
-                        height: 30,
-                        marginRight: 1,
-                        marginLeft: 1,
-                      }}
-                      color="info"
-                    />
-                  </Link>
-                </ListItemButton>
-              </ListItem>
-            </Box>
-          </Box>
+          <Link href="/user">
+            <PermIdentityIcon
+              sx={{
+                width: 30,
+                height: 30,
+              }}
+              color="info"
+            />
+          </Link>
         </Box>
       </Sheet>
     </Box>
