39ed6a778d7eace8a98a034a3d3e9dc0847c7a4f
tak <taku1011ht@gmail.com>
2023-07-18 19:08:57 +0900

    typeFix

--- src/app/room/[id]/_components/RoomLeft/MemberList/MemberTechArea.tsx
+++ src/app/room/[id]/_components/RoomLeft/MemberList/MemberTechArea.tsx
@@ -12,10 +12,14 @@ export const MemberTechArea = (props: Props) => {
   return (
     <Grid container sx={{ ml: 4, mt: 3 }} xs={6} direction={'row'}>
       {techs?.map((tech) => (
-        <Chip sx={{ mt: 0.5 }} label={tech.name} key={tech.id} />
+        <Chip sx={{ mt: 0.5 }} label={tech.tech_tag_id} key={tech.language} />
       ))}
       {frameworks?.map((framework) => (
-        <Chip sx={{ mt: 0.5 }} label={framework.name} key={framework.id} />
+        <Chip
+          sx={{ mt: 0.5 }}
+          label={framework.framework_id}
+          key={framework.tech_tag_id}
+        />
       ))}
     </Grid>
   )
