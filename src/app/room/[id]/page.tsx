2a8a27952ac6b60b109fb97761874b9c669a3612
Hiroto2002 <hiroto2002h14@icloud.com>
2023-08-12 03:40:09 +0900

    feat:GoogleSignup

--- src/app/room/[id]/page.tsx
+++ src/app/room/[id]/page.tsx
@@ -1,63 +1,57 @@
-// 'use client'
+'use client'
 
-// import { Header } from '@/components/layouts/Header'
-// import React from 'react'
-// import { Box, Grid } from '@/lib/mui/muiRendering'
-// import { RoomCenter } from './_components/RoomCenter/Index'
-// import { RoomInfo } from './types/room'
-// import { useTab } from '@/hooks/useTab'
-// import { RoomLeft } from './_components/RoomLeft/Index'
-// import { RoomRight } from './_components/RoomRight/Index'
-// import useSWR from 'swr'
-// import { fetcher } from '@/services/fetcher'
+import { Header } from '@/components/layouts/Header'
+import React from 'react'
+import { Box, Grid } from '@/lib/mui/muiRendering'
+import { RoomCenter } from './_components/RoomCenter/Index'
+import { RoomInfo } from './types/room'
+import { useTab } from '@/hooks/useTab'
+import { RoomLeft } from './_components/RoomLeft/Index'
+import { RoomRight } from './_components/RoomRight/Index'
 
-// type Props = {}
+type Props = {}
 
-// const RoomDetail = (props: Props) => {
-//   const { data, error } = useSWR<any>('/api/Room', fetcher)
-//   const { tab, handleSetTab } = useTab()
+const RoomDetail = (props: Props) => {
+  const { tab, handleSetTab } = useTab()
 
-//   const users = data?.users === undefined ? [] : data.users
-//   const roomInfo = data
+  return (
+    <Box
+      sx={{
+        bgcolor: '#fff',
+        maxHeight: '100vh',
+        overflowY: 'hidden',
+      }}
+    >
+      <Header />
 
-//   return (
-//     <Box
-//       sx={{
-//         bgcolor: '#fff',
-//         maxHeight: '100vh',
-//         overflowY: 'hidden',
-//       }}
-//     >
-//       <Header />
+      <Grid
+        container
+        justifyContent={'center'}
+        sx={{
+          maxHeight: '90vh',
+          marginTop: '-16px',
+        }}
+      >
+        {/* member */}
+        {/* <Grid item xs>
+          {data && roomInfo && (
+            <RoomLeft
+              roomInfo={roomInfo}
+              tab={tab}
+              handleSetTab={handleSetTab}
+            />
+          )}
+        </Grid> */}
 
-//       <Grid
-//         container
-//         justifyContent={'center'}
-//         sx={{
-//           maxHeight: '90vh',
-//           marginTop: '-16px',
-//         }}
-//       >
-//         {/* member */}
-//         <Grid item xs>
-//           {data && roomInfo && (
-//             <RoomLeft
-//               roomInfo={roomInfo}
-//               tab={tab}
-//               handleSetTab={handleSetTab}
-//             />
-//           )}
-//         </Grid>
+        {/* chat */}
+        <RoomCenter />
+        {/* chat info */}
+        <Grid item xs>
+          {/* <RoomRight users={users} /> */}
+        </Grid>
+      </Grid>
+    </Box>
+  )
+}
 
-//         {/* chat */}
-//         <RoomCenter />
-//         {/* chat info */}
-//         <Grid item xs>
-//           <RoomRight users={users} />
-//         </Grid>
-//       </Grid>
-//     </Box>
-//   )
-// }
-
-// export default RoomDetail
+export default RoomDetail
