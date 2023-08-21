8d5c4243c9ae16ff50868008ef156f797038031b
taku10101 <taku1011ht@gmail.com>
2023-08-21 18:55:18 +0900

    errorFix

--- src/types/RatingInterface.ts
+++ src/types/RatingInterface.ts
@@ -1,4 +1,4 @@
-import { Domain_AccountRateResponse } from '../../api/@types'
+import { Domain_AccountRateResponse } from '@/api/@types'
 
 export interface RatingInterface {
   fetchAll(): Promise<Domain_AccountRateResponse[]>
