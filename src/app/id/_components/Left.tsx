f6bdfbae1c4b7f17ed0dac52ee4fe297ce15cd31
Hiroto2002 <hiroto2002h14@icloud.com>
2023-08-21 21:28:39 +0900

    delete:detail

--- src/app/id/_components/Left.tsx
+++
@@ -1,6 +0,0 @@
-import { Box } from '@/lib/mui/muiRendering'
-import React from 'react'
-
-export const Left = () => {
-  return <Box sx={{ width: '250px' }}></Box>
-}
--- src/types/HackathonInterface.ts
+++ src/types/HackathonInterface.ts
@@ -2,5 +2,4 @@ import { Domain_HackathonResponses } from '@/api/@types'
 
 export interface HackathonInterface {
   fetchAll(): Promise<Domain_HackathonResponses[]>
-  fetchById(hackathon_id: string): Promise<Domain_HackathonResponses>
 }
