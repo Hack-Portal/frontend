987da62493bd963192d057a18cc3674146512645
taku10101 <taku1011ht@gmail.com>
2023-08-22 21:50:09 +0900

    d_pageProps

--- src/app/room/page.tsx
+++ src/app/room/page.tsx
@@ -63,69 +63,6 @@ const Room = () => {
     },
   ]
 
-  const userRatingInfo: UserRatingInfo[] = [
-    {
-      id: '1',
-      name: 'HackMan',
-      icon: 'image/user1.png',
-      rating: 20,
-    },
-    {
-      id: '2',
-      name: 'HackMania',
-      icon: 'image/user2.png',
-      rating: 18,
-    },
-    {
-      id: '3',
-      name: 'HackNinja',
-      icon: 'image/user3.png',
-      rating: 17,
-    },
-    {
-      id: '4',
-      name: 'Woker',
-      icon: 'image/user4.png',
-      rating: 16,
-    },
-    {
-      id: '5',
-      name: 'HapyHacker',
-      icon: 'image/user5.png',
-      rating: 14,
-    },
-    {
-      id: '6',
-      name: 'BlueCoder',
-      icon: 'image/user6.png',
-      rating: 13,
-    },
-
-    {
-      id: '7',
-      name: 'purpleCoder',
-      icon: 'image/user7.png',
-      rating: 12,
-    },
-    {
-      id: '8',
-      name: 'Gopher',
-      icon: 'image/user8.png',
-      rating: 11,
-    },
-    {
-      id: '9',
-      name: 'NineHacker',
-      icon: 'image/user9.png',
-      rating: 10,
-    },
-    {
-      id: '10',
-      name: 'TenHacker',
-      icon: 'image/user10.png',
-      rating: 9,
-    },
-  ]
   const techStacks: TechStack[] = [
     {
       id: '1',
