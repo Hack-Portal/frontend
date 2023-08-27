01cff3ca679393ac180afdced19c5c8e95427177
Hiroto2002 <hiroto2002h14@icloud.com>
2023-08-28 02:22:09 +0900

    feat:main visual

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
 import { Header } from '@/components/layouts/Header'
+import { MainVisual } from './_components/MainVisual'
 
 const Home = async () => {
   
   return (
+    <>
       <Header />
+      <MainVisual/>
+      {/* <About/>
+      <Issues/>
+      <Tutorial/>
+      <Footer/> */}
+    </>
   )
 }
 export default Home
