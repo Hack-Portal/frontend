e40896c6f71809297dcbd1b0dcf0415319c8d424
hiroto2002 <hiroto2002h14@icloud.com>
2023-09-09 12:15:08 +0900

    feat:firebase:mock repository

--- src/repositories/mocks/FirebaseMockRepository.ts
+++ src/repositories/mocks/FirebaseMockRepository.ts
@@ -1,25 +1,65 @@
-import { auth, db } from '@/lib/firebase/client'
-import {
-  AuthProvider,
-  User,
-  browserLocalPersistence,
-  browserSessionPersistence,
-  createUserWithEmailAndPassword,
-  onAuthStateChanged,
-  setPersistence,
-  signInWithEmailAndPassword,
-  signInWithPopup,
-} from 'firebase/auth'
-import { FirebaseError } from 'firebase/app'
-import { onSnapshot, doc, collection } from 'firebase/firestore'
+import { FirebaseInterface } from '@/types/FirebaseInterface'
 import { Chat } from '@/types/chat'
-import api from '@/api/$api'
-import aspida from '@aspida/axios'
-import axios from 'axios'
-import { Domain_GetRoomResponse } from '@/api/@types'
-import { EmailSignInFormData } from '@/app/signin/types/formData'
-import { API_URL } from '@/constants/API_URL'
+import { User, AuthProvider } from 'firebase/auth'
 
-export class FirebaseMockRepository {
- 
+const dummyUser: Partial<User> = {
+  uid: 'dummyUid',
+  getIdToken: () => Promise.resolve('dummyToken'),
+}
+export class FirebaseMockRepository implements FirebaseInterface {
+  getCurrentUser(): Promise<User | null> {
+    return Promise.resolve(dummyUser as User)
+  }
+
+  getUId(): Promise<string> {
+    return Promise.resolve('dummyUid')
+  }
+
+  getToken(): Promise<string> {
+    return Promise.resolve('dummyToken')
+  }
+
+  SNSSignIn(provider: AuthProvider): Promise<User> {
+    return Promise.resolve(dummyUser as User)
+  }
+
+  emailSignIn(email: string, password: string): Promise<string | User> {
+    return Promise.resolve('dummyUid')
+  }
+
+  emailSignUp(email: string, password: string): Promise<string | User> {
+    return Promise.resolve('dummyUid')
+  }
+
+  signOut(): Promise<void> {
+    return Promise.resolve()
+  }
+
+  deleteAccount(): Promise<void> {
+    return Promise.resolve()
+  }
+
+  fetchChatMessages(
+    roomId: string,
+    callback: (chats: Chat[]) => void,
+  ): (() => void) | undefined {
+    const dummyChats: Chat[] = [
+      {
+        CreatedAt: String(new Date().getTime()),
+        Message: 'hello',
+        UID: 'dummyUid',
+        // 他に必要なフィールド
+      },
+      {
+        CreatedAt: String(new Date().getTime()-1000*60*60*24*3),
+        Message: 'hello2',
+        UID: 'dummyUid2',
+        // 他に必要なフィールド
+      },
+    ]
+    callback(dummyChats)
+    return () => {
+      /* 解除する処理 */
+    }
+  }
 }
--- src/types/FirebaseInterface.ts
+++ src/types/FirebaseInterface.ts
@@ -1,9 +1,8 @@
 import { AuthProvider, User } from 'firebase/auth'
 import { Chat } from './chat'
-import { Domain_GetRoomResponse } from '@/api/@types'
 
 export interface FirebaseInterface {
-  getCurentUser(): Promise<User | null>
+  getCurrentUser(): Promise<User | null>
   getUId(): Promise<string>
   getToken(): Promise<string>
   SNSSignIn(provider: AuthProvider): Promise<User>
@@ -13,6 +12,6 @@ export interface FirebaseInterface {
   deleteAccount(): Promise<void>
   fetchChatMessages(
     roomId: string,
-    callback: (chats: Chat[]) => void
+    callback: (chats: Chat[]) => void,
   ): (() => void) | undefined
 }
