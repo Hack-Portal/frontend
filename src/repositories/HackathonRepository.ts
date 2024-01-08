bcfca841dbd85db5b649a718968af660a3ed9dd2
taku10101 <taku1011ht@gmail.com>
2024-01-08 18:20:39 +0900

    interface add

--- src/repositories/HackathonRepository.ts
+++ src/repositories/HackathonRepository.ts
@@ -22,7 +22,6 @@ export class HackathonRepository implements HackathonInterface {
    * @returns  ハッカソン一覧
    */
   public async fetchAll() {
-    console.log('HackathonRepository.fetchAll()-start')
     try {
       console.log('fetch-path:', API_URL)
       const client = api(
@@ -38,7 +37,26 @@ export class HackathonRepository implements HackathonInterface {
       const response = await client.hackathons.get({
         query: { page_size: 10, page_id: 1 },
       })
-      console.log('HackathonRepository.fetchAll()-end')
+      return response.body as Response_GetHackathon[]
+    } catch (error) {
+      // エラー処理
+      console.error('APIリクエストエラー:', error)
+      throw error
+    }
+  }
+  public async create() {
+    try {
+      const client = api(
+        // aspida(axios, { baseURL: 'https://api.seaffood.com/current/v1' }),
+        aspida(axios, {
+          baseURL: API_URL,
+          headers: { authorization: this.authorization },
+        }),
+      )
+
+      this.authorization = getAuthorizationHeader()
+
+      const response = await client.hackathons.create({})
       return response.body as Response_GetHackathon[]
     } catch (error) {
       // エラー処理
--- src/types/HackathonInterface.ts
+++ src/types/HackathonInterface.ts
@@ -1,5 +1,6 @@
-import { Response_GetHackathon } from '@/api/@types'
+import { Response_GetHackathon, Response_CreateHackathon } from '@/api/@types'
 
 export interface HackathonInterface {
   fetchAll(): Promise<Response_GetHackathon[]>
+  create(): Promise<Response_CreateHackathon>
 }
