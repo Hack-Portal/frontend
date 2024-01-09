a3d88553c897c8320b37b4311c1f0437544877e7
murasame <oogiriminister@gmail.com>
2024-01-09 15:52:12 +0900

    fix: loading env

--- src/constants/API_URL.ts
+++ src/constants/API_URL.ts
@@ -1,18 +1,14 @@
-const defaultLocalPath :string = "http://localhost:8080"
-
-let requestBaseUrl :string
+let requestBaseUrl :string = "http://localhost:8080"
 switch (process.env.ENVIRONMENT) {
   case "local":
-    requestBaseUrl = process.env.NEXT_PUBLIC_LOCAL_API_URL ? process.env.NEXT_PUBLIC_LOCAL_API_URL : defaultLocalPath
+    requestBaseUrl = process.env.NEXT_PUBLIC_LOCAL_API_URL ? process.env.NEXT_PUBLIC_LOCAL_API_URL : requestBaseUrl
     break
   case "stage":
-    requestBaseUrl = process.env.NEXT_PUBLIC_STAGE_API_URL ? process.env.NEXT_PUBLIC_STAGE_API_URL : defaultLocalPath
+    requestBaseUrl = process.env.NEXT_PUBLIC_STAGE_API_URL ? process.env.NEXT_PUBLIC_STAGE_API_URL : requestBaseUrl
     break
   case "product":
-    requestBaseUrl = process.env.NEXT_PUBLIC_PRODUCT_API_URL ? process.env.NEXT_PUBLIC_PRODUCT_API_URL : defaultLocalPath 
+    requestBaseUrl = process.env.NEXT_PUBLIC_PRODUCT_API_URL ? process.env.NEXT_PUBLIC_PRODUCT_API_URL : requestBaseUrl 
     break
-  default:
-    requestBaseUrl = defaultLocalPath
 }
 
 export const API_URL = requestBaseUrl
