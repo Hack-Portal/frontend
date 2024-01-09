3fe377d8bc3d81bdbdc8a48e57c191d8d0cd2338
murasame <oogiriminister@gmail.com>
2024-01-09 15:49:50 +0900

    Envの変更と本番環境ワークフローの追加

--- src/constants/API_URL.ts
+++ src/constants/API_URL.ts
@@ -1,1 +1,18 @@
-export  const API_URL = process.env.NEXT_PUBLIC_ENVIRONMENT === "local" ? process.env.NEXT_PUBLIC_TEST_URL : process.env.NEXT_PUBLIC_API_URL as string
+const defaultLocalPath :string = "http://localhost:8080"
+
+let requestBaseUrl :string
+switch (process.env.ENVIRONMENT) {
+  case "local":
+    requestBaseUrl = process.env.NEXT_PUBLIC_LOCAL_API_URL ? process.env.NEXT_PUBLIC_LOCAL_API_URL : defaultLocalPath
+    break
+  case "stage":
+    requestBaseUrl = process.env.NEXT_PUBLIC_STAGE_API_URL ? process.env.NEXT_PUBLIC_STAGE_API_URL : defaultLocalPath
+    break
+  case "product":
+    requestBaseUrl = process.env.NEXT_PUBLIC_PRODUCT_API_URL ? process.env.NEXT_PUBLIC_PRODUCT_API_URL : defaultLocalPath 
+    break
+  default:
+    requestBaseUrl = defaultLocalPath
+}
+
+export const API_URL = requestBaseUrl
