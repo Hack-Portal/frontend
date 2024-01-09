7e9bfa374cc2325fa333faaf423090451c0f530f
Hiroto2002 <hiroto2002h14@icloud.com>
2024-01-09 13:41:06 +0900

    remove:consolo.log

--- src/repositories/HackathonRepository.ts
+++ src/repositories/HackathonRepository.ts
@@ -22,9 +22,7 @@ export class HackathonRepository implements HackathonInterface {
    * @returns  ハッカソン一覧
    */
   public async fetchAll() {
-    console.log('HackathonRepository.fetchAll()-start')
     try {
-      console.log('fetch-path:', API_URL)
       const client = api(
         // aspida(axios, { baseURL: 'https://api.seaffood.com/current/v1' }),
         aspida(axios, {
@@ -38,7 +36,6 @@ export class HackathonRepository implements HackathonInterface {
       const response = await client.hackathons.get({
         query: { page_size: 10, page_id: 1 },
       })
-      console.log('HackathonRepository.fetchAll()-end')
       return response.body as Response_GetHackathon[]
     } catch (error) {
       // エラー処理
