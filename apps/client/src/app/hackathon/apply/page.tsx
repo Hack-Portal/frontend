a56d0cf245518ce8a1c8bf3392be20d32baa836b
Hiroto2002 <hiroto2002h14@icloud.com>
2024-03-23 01:03:53 +0900

    fix:ui

--- apps/client/src/app/hackathon/apply/page.tsx
+++ apps/client/src/app/hackathon/apply/page.tsx
@@ -1,22 +1,29 @@
 import React from 'react'
-import { Grid, TextField, Button } from '@mui/material'
+import { Grid, TextField, Button, InputLabel, Typography } from '@mui/material'
 
 const page = () => {
   return (
     <Grid
       container
-      justifyContent={'center'}
-      alignItems={'center'}
-      height={'59vh'}
-      direction={'column'}
-      gap={3}
+      justifyContent="center"
+      alignItems="center"
+      height="59vh"
+      direction="column"
+      gap={5}
     >
       <Grid item pt={15}>
-        <TextField
-          variant="outlined"
-          placeholder="ハッカソンのリンクを入力"
-          sx={{ width: '80vw' }}
-        />
+        <InputLabel
+          sx={{ display: 'flex', flexDirection: 'column', gap: '10px' }}
+        >
+          <Typography fontWeight={700} fontSize="1.2rem">
+            ハッカソンのリンク
+          </Typography>
+          <TextField
+            variant="outlined"
+            placeholder="リンクを貼り付け"
+            sx={{ width: '80vw', maxWidth: '500px' }}
+          />
+        </InputLabel>
       </Grid>
       <Grid item>
         <Button variant="contained" color="primary">
